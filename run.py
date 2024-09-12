912638ac26c81c857cbb59bdfdf751a6ce2fe3b6
Gilderlan silva da cruz <lansilva007@gmail.com>
2024-09-11 21:57:41 -0300

    Alteramos o html e css para mostra um item abaixo do outro e links para os grupos do whatsapp e telegram os produtos estão visualmente melhor.

--- run.py
+++ run.py
@@ -2,5 +2,14 @@ from app import create_app
 
 app = create_app()
 
+
+
+@app.after_request
+def add_header(response):
+    # Desabilitar cache de arquivos estáticos
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+
 if __name__ == '__main__':
     app.run(debug=True)
