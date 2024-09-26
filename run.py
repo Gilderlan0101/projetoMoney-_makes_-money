0b92c7f6d3b090567d4f35e08c471c35cd51e62b
Gilderlan silva da cruz <lansilva007@gmail.com>
2024-09-26 16:57:57 -0300

    quarta tentativa de fazer deploy

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=5000) # sempre usar a port 5000 para o projeto
+    app.run(host='0.0.0.0', port=5000)
+ # sempre usar a port 5000 para o projeto
